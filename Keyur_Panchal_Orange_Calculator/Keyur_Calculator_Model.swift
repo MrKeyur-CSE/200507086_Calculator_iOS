5a9911bca25989b16e987df47696b5a27da93f97
MrKeyur-CSE <keyur.developer.cse@gmail.com>
2022-02-01 23:24:48 -0500

    Basic Mathematics

---
+++ Keyur_Panchal_Orange_Calculator/Keyur_Calculator_Model.swift
@@ -0,0 +1,43 @@
+//
+//  Keyur_Calculator_Model.swift
+//  Keyur_Panchal_Orange_Calculator
+//
+//  Created by Keyur Panchal on 2022-02-01.
+//
+
+import Foundation
+
+class Keyur_Calculator_Model {
+    var firstNumber: Double?
+    var secondNumber: Double?
+    var operation: String?
+    
+    func updateNumber(newNumber: Double){
+        if (firstNumber == nil){
+            firstNumber = newNumber
+        } else if (operation == nil){
+            firstNumber = newNumber
+        } else {
+            secondNumber = newNumber
+        }
+    }
+    
+    func getResult() -> Double {
+        var result: Double
+        switch (operation) {
+        case "+":
+            result = firstNumber! + secondNumber!
+        case "-":
+            result = firstNumber! - secondNumber!
+        case "*":
+            result = firstNumber! * secondNumber!
+        case "/":
+            result = firstNumber! / secondNumber!
+            
+        default:
+            result = 0.0
+        }
+        
+        return result
+    }
+}
--- Keyur_Panchal_Orange_Calculator/ViewController.swift
+++ Keyur_Panchal_Orange_Calculator/ViewController.swift
@@ -8,11 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var numberDisplay: UILabel!
+    
+    //MARK: - Other Veriables
+    
+    var calcultorModel = Keyur_Calculator_Model()
+    var startNewNumber = true
+    
+    
+    //MARK: - Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - IBActions
+    
+    @IBAction func buttonTouched(_ sender: UIButton) {
+        print("Button Touched")
+        
+        let buttonText = sender.titleLabel?.text
+        print(buttonText!)
+        
+        var currentValue = numberDisplay.text!
+        
+        switch (buttonText!) {
+        case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+            print(buttonText!)
+            if(startNewNumber) {
+                currentValue = buttonText!
+            }else{
+                currentValue = currentValue + buttonText!
+            }
+            
+            startNewNumber = false
+            calcultorModel.updateNumber(newNumber: Double(currentValue)!)
+            //do something
+            
+        case "+":
+            calcultorModel.operation = buttonText
+            startNewNumber = true
+            //accept second number after this
+        case "-":
+            calcultorModel.operation = buttonText
+            startNewNumber = true
+        case "*":
+            calcultorModel.operation = buttonText
+            startNewNumber = true
+        case "/":
+            calcultorModel.operation = buttonText
+            startNewNumber = true
+                
+        case "=":
+            if (calcultorModel.secondNumber == nil) {
+                calcultorModel.updateNumber(newNumber: Double(currentValue)!)
+            }
+            currentValue = String(calcultorModel.getResult())
+            startNewNumber = true
+            // get result and display result it on display panel
+            
+        default:
+            print(buttonText!)
+            //do something default
+        }
+        
+        numberDisplay.text = currentValue
+    }
+    
 
 
 }
